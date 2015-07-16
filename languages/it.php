ce3fdbf1904c112a4592606b9c8c814f665498d5
Guido Longoni <guido.longoni@edison.it>
2015-07-16 15:29:33 +0200

    Fixed banned email domain detection (Issue #55 by @human-inhabitant). Fixed typho in it.php and char encoding problems

--- languages/it.php
+++ languages/it.php
@@ -2,78 +2,78 @@
 
 $lang = array();
 
-$lang['user_blocked'] = "Questo utente √® temporaneamente bloccato dal sistema.";
+$lang['user_blocked'] = "Questo utente Ë temporaneamente bloccato dal sistema.";
 
-$lang['username_short'] = "Il nome utente √® troppo breve.";
-$lang['username_long'] = "Il nome utente √® troppo lungo.";
+$lang['username_short'] = "Il nome utente Ë troppo breve.";
+$lang['username_long'] = "Il nome utente Ë troppo lungo.";
 $lang['username_invalid'] = "Nome utente non valido.";
-$lang['username_banned'] = "Questo nome utente non √® consentito.";
+$lang['username_banned'] = "Questo nome utente non Ë consentito.";
 
 $lang['username_password_invalid'] = "Nome utente / password non sono validi.";
 $lang['username_password_incorrect'] = "Nome utente / password non sono corretti.";
-$lang['remember_me_invalid'] = "Il campo 'Ricordami' non √® valido.";
+$lang['remember_me_invalid'] = "Il campo 'Ricordami' non Ë valido.";
 
-$lang['password_short'] = "La password √® troppo breve.";
-$lang['password_long'] = "La password √® troppo lunga.";
+$lang['password_short'] = "La password Ë troppo breve.";
+$lang['password_long'] = "La password Ë troppo lunga.";
 $lang['password_invalid'] = "La password deve contenere almeno un carattere minuscolo, uno maiuscolo, e almeno una cifra.";
 $lang['password_nomatch'] = "Le password non corrispondono.";
 $lang['password_changed'] = "Password modificata correttamente.";
-$lang['password_incorrect'] = "La password corrente non √® corretta.";
+$lang['password_incorrect'] = "La password corrente non Ë corretta.";
 $lang['password_notvalid'] = "Password non valida.";
 
-//$lang['newpassword_short'] = "La nuova password √® troppo breve.";
-//$lang['newpassword_long'] = "La nuova password √® troppo lunga.";
+//$lang['newpassword_short'] = "La nuova password Ë troppo breve.";
+//$lang['newpassword_long'] = "La nuova password Ë troppo lunga.";
 //$lang['newpassword_invalid'] = "La nuova password deve contenere almeno un carattere minuscolo, uno maiuscolo, e almeno una cifra.";
 $lang['newpassword_nomatch'] = "Le nuove password non corrispondono.";
 $lang['newpassword_match'] = "La nuova password coincide con la precedente.";
 
 $lang['email_short'] = "Indirizzo e-mail troppo breve.";
 $lang['email_long'] = "Indirizzo e-mail troppo lungo.";
-$lang['email_invalid'] = "L'indirizzo email non √® valido.";
-$lang['email_incorrect'] = "L'indirizzo e-mail non √® corretto.";
-$lang['email_banned'] = "Questo indirizzo e-mail non √® permesso.";
+$lang['email_invalid'] = "L'indirizzo email non Ë valido.";
+$lang['email_incorrect'] = "L'indirizzo e-mail non Ë corretto.";
+$lang['email_banned'] = "Questo indirizzo e-mail non Ë permesso.";
 $lang['email_changed'] = "Indirizzo e-mail modificato con successo.";
 
 $lang['newemail_match'] = "Il nuovo indirizzo e-mail corrisponde al precedente.";
 
-$lang['account_inactive'] = "L'account non √® ancora stato attivato.";
+$lang['account_inactive'] = "L'account non Ë ancora stato attivato.";
 $lang['account_activated'] = "Account attivato.";
 $lang['account_deleted'] = "Account eliminato.";
 
-$lang['logged_in'] = "Ora si √® collegati.";
+$lang['logged_in'] = "Ora si Ë collegati.";
 //$lang['logged_out'] = "Disconnessione effettuata.";
 
-$lang['system_error'] = "Si √® verificato un errore di sistema. Si prega di riprovare.";
+$lang['system_error'] = "Si Ë verificato un errore di sistema. Si prega di riprovare.";
 
 $lang['register_success'] = "Account creato. E-mail di attivazione inviata all'indirizzo indicato.";
-$lang['username_taken'] = "Questo nome utente esiste gi√†.";
-$lang['email_taken'] = "L'indirizzo e-mail √® gi√† in uso.";
+$lang['username_taken'] = "Questo nome utente esiste gi‡.";
+$lang['email_taken'] = "L'indirizzo e-mail Ë gi‡ in uso.";
 
 $lang['key_invalid'] = "Chiave non valida.";
 
-$lang['resetkey_invalid'] = "Codice di ripristino non valido."
+$lang['resetkey_invalid'] = "Codice di ripristino non valido.";
 $lang['resetkey_incorrect'] = "Codice di ripristino incorretto.";
 $lang['resetkey_expired'] = "Codice di ripristino scaduto.";
 $lang['password_reset'] = "Password ripristinata con successo.";
 
-$lang['activationkey_invalid'] = "La chiave di attivazione non √® valida.";
-$lang['activationkey_incorrect'] = "La chiave di attivazione non √® corretta.";
-$lang['activationkey_expired'] = "La chiave di attivazione √® scaduta.";
+$lang['activationkey_invalid'] = "La chiave di attivazione non Ë valida.";
+$lang['activationkey_incorrect'] = "La chiave di attivazione non Ë corretta.";
+$lang['activationkey_expired'] = "La chiave di attivazione Ë scaduta.";
 
 $lang['reset_requested'] = "Richiesta di ripristino della password inviata all'indirizzo e-mail indicato.";
-$lang['reset_exists'] = "Una richiesta di ripristino esiste gi√†.";
+$lang['reset_exists'] = "Una richiesta di ripristino esiste gi‡.";
 
-$lang['already_activated'] = "L'account √® gi√† attivato.";
-$lang['activation_sent'] = "L'e-mail di attivazione √® stata inviata.";
-//$lang['activation_exists'] = "Una e-mail di attivazione √® gi√† stata inviata.";
+$lang['already_activated'] = "L'account Ë gi‡ attivato.";
+$lang['activation_sent'] = "L'e-mail di attivazione Ë stata inviata.";
+//$lang['activation_exists'] = "Una e-mail di attivazione Ë gi‡ stata inviata.";
 
 $lang['email_activation_subject'] = "%s - Attivare account";
-$lang['email_activation_body'] = "Salve. <br/> Per essere in grado di accedere al proprio account si richiede di attivarlo cliccando sul seguente link: <strong><a href=\"%1$s/%2$s\">%1$s/%2$s</a></strong><br/><br/> Sar√† quindi necessario utilizzare la seguente chiave di attivazione: <strong>%3$s</strong><br/><br/> Se non si √® effettuata una registrazione su %1$s recentemente, allora questo messaggio √® stato inviato per errore, si prega di ignorarlo.";
-$lang['email_activation_altbody'] = "Salve. \n\n Per essere in grado di accedere al proprio account si richiede di attivarlo cliccando sul seguente link: \n %1$s/%2$s\n\n Sar√† quindi necessario utilizzare la seguente chiave di attivazione: %3$s\n\n Se non si √® effettuata una registrazione su %1$s recentemente, allora questo messaggio √® stato inviato per errore, si prega di ignorarlo. ";
+$lang['email_activation_body'] = "Salve. <br/> Per essere in grado di accedere al proprio account si richiede di attivarlo cliccando sul seguente link: <strong><a href=\"%1\$s/%2\$s\">%1\$s/%2\$s</a></strong><br/><br/> Sar&agrave; quindi necessario utilizzare la seguente chiave di attivazione: <strong>%3\$s</strong><br/><br/> Se non si &egrave; effettuata una registrazione su %1\$s recentemente, allora questo messaggio &egrave; stato inviato per errore, si prega di ignorarlo.";
+$lang['email_activation_altbody'] = "Salve. \n\n Per essere in grado di accedere al proprio account si richiede di attivarlo cliccando sul seguente link: \n %1\$s/%2\$s\n\n Sar‡ quindi necessario utilizzare la seguente chiave di attivazione: %3\$s\n\n Se non si Ë effettuata una registrazione su %1\$s recentemente, allora questo messaggio Ë stato inviato per errore, si prega di ignorarlo. ";
 
 $lang['email_reset_subject'] = "%s - Richiesta di reset della password";
-$lang['email_reset_body'] = "Salve. <br/> Per ripristinare la password cliccare sul link seguente: <br/><br/><strong><a href=\"%1$s/%2$s\">%1$s/%2$s</a></strong><br/><br/> Sar√† quindi necessario utilizzare la seguente chiave di reset: <strong>%3$s</strong><br/><br/> Se non si √® richiesto un tasto di reset della password su %1$s recentemente, allora questo messaggio √® stato inviato per errore, si prega di ignorarlo. ";
-$lang['email_reset_altbody'] = "Salve. \n\n Per ripristinare la password cliccare sul link seguente: \n %1$s/%2$s\n\n Sar√† quindi necessario utilizzare la seguente chiave di reset: %3$s\n\n Se non si √® richiesto un tasto di reset della password su %1$s recentemente, allora questo messaggio √® stato inviato per errore, si prega di ignorarlo. ";
+$lang['email_reset_body'] = "Salve. <br/> Per ripristinare la password cliccare sul link seguente: <br/><br/><strong><a href=\"%1\$s/%2\$s\">%1\$s/%2\$s</a></strong><br/><br/> Sar&agrave; quindi necessario utilizzare la seguente chiave di reset: <strong>%3\$s</strong><br/><br/> Se non si &egrave; richiesto un tasto di reset della password su %1\$s recentemente, allora questo messaggio &egrave; stato inviato per errore, si prega di ignorarlo. ";
+$lang['email_reset_altbody'] = "Salve. \n\n Per ripristinare la password cliccare sul link seguente: \n %1\$s/%2\$s\n\n Sar‡ quindi necessario utilizzare la seguente chiave di reset: %3\$s\n\n Se non si Ë richiesto un tasto di reset della password su %1\$s recentemente, allora questo messaggio Ë stato inviato per errore, si prega di ignorarlo. ";
 
 ?>
 
